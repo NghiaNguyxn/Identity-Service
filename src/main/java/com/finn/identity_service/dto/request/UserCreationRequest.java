6ac128e044f0ef4ed1925b0b3e149a1f086cd425
NghiaNguyxn <nghia2592005@gmail.com>
2026-01-20 18:20:13 +0700

    feat: add custom validation and extract annotation attributes

--- src/main/java/com/finn/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/finn/identity_service/dto/request/UserCreationRequest.java
@@ -25,6 +25,6 @@ public class UserCreationRequest {
 
     String lastName;
 
-    @DobConstraint(min = 18, message = "INVALID_DOB")
+    @DobConstraint(min = 16, message = "INVALID_DOB")
     LocalDate dob;
 }
--- src/main/java/com/finn/identity_service/dto/request/UserUpdateRequest.java
+++ src/main/java/com/finn/identity_service/dto/request/UserUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.finn.identity_service.dto.request;
 
+import com.finn.identity_service.validator.DobConstraint;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -15,6 +16,8 @@ public class UserUpdateRequest {
     String password;
     String firstName;
     String lastName;
+
+    @DobConstraint(min = 16, message = "INVALID_DOB")
     LocalDate dob;
 
     List<String> roles;
