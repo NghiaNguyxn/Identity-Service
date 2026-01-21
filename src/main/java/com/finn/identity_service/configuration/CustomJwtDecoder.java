4c5b45eb733268ea1940344bc3de2f8188111543
NghiaNguyxn <nghia2592005@gmail.com>
2026-01-21 10:32:08 +0700

    refactor: custom JwtDecoder workflow

--- src/main/java/com/finn/identity_service/configuration/CustomJwtDecoder.java
+++ src/main/java/com/finn/identity_service/configuration/CustomJwtDecoder.java
@@ -1,55 +1,37 @@
 package com.finn.identity_service.configuration;
 
-import com.finn.identity_service.dto.request.IntrospectRequest;
-import com.finn.identity_service.service.AuthenticationService;
-import com.nimbusds.jose.JOSEException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import com.finn.identity_service.repository.InvalidatedTokenRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.text.ParseException;
-import java.util.Objects;
-
-@RequiredArgsConstructor
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
 
-    @Value("${jwt.signerKey}")
-    private String signerKey;
+    private final JwtDecoder delegate;
+    private final InvalidatedTokenRepository invalidatedTokenRepository;
 
-    private final AuthenticationService authenticationService;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
+    public CustomJwtDecoder(
+            @Qualifier("nimbusJwtDecoder") JwtDecoder delegate,
+            InvalidatedTokenRepository invalidatedTokenRepository
+    ){
+        this.delegate = delegate;
+        this.invalidatedTokenRepository = invalidatedTokenRepository;
+    }
 
     @Override
     public Jwt decode(String token) throws JwtException {
 
-        try{
-            var response = authenticationService.introspect(IntrospectRequest.builder()
-                            .token(token)
-                    .build());
+        // Verify signature, exp, ...
+        Jwt jwt = delegate.decode(token);
 
-            if(!response.isValid())
-                throw new JwtException("Token Invalid");
-
-        } catch (JOSEException | ParseException e) {
-            throw new RuntimeException(e);
+        if(jwt.getId() != null
+                && invalidatedTokenRepository.existsById(jwt.getId())) {
+            throw new JwtException("Token has been invalidated");
         }
 
-        if(Objects.isNull(nimbusJwtDecoder)) {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS256");
-            nimbusJwtDecoder = NimbusJwtDecoder
-                    .withSecretKey(secretKeySpec)
-                    .macAlgorithm(MacAlgorithm.HS256)
-                    .build();
-        }
-
-        return nimbusJwtDecoder.decode(token);
+        return jwt;
     }
 }
