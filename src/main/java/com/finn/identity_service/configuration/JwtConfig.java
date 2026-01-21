b38db5508b4dcba4bc2dd57ab5910097ecd55706
NghiaNguyxn <162450572+NghiaNguyxn@users.noreply.github.com>
2026-01-21 10:41:43 +0700

    refactor: custom JwtDecoder workflow (#5)

---
+++ src/main/java/com/finn/identity_service/configuration/JwtConfig.java
@@ -0,0 +1,30 @@
+package com.finn.identity_service.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class JwtConfig {
+
+    @Bean("nimbusJwtDecoder")
+    public JwtDecoder jwtDecoder(@Value("${jwt.signerKey}") String signerKey){
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS256");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
