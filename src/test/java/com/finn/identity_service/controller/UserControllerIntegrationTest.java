87ac010e07ae7380f0320d297d9c765cda2090f7
NghiaNguyxn <nghia2592005@gmail.com>
2026-01-23 23:14:23 +0700

    test: add integration test with testcontainers

---
+++ src/test/java/com/finn/identity_service/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,94 @@
+package com.finn.identity_service.controller;
+
+import com.finn.identity_service.dto.request.UserCreationRequest;
+import com.finn.identity_service.dto.response.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerIntegrationTest {
+
+    @Container
+    static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:latest");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UserCreationRequest request;
+    private UserResponse userResponse;
+
+    @BeforeEach
+    void initData(){
+        LocalDate dob = LocalDate.of(2000, 1, 1);
+
+        request = UserCreationRequest.builder()
+                .username("test")
+                .password("12345678")
+                .firstName("Test")
+                .lastName("User")
+                .dob(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("1fa3587d6084")
+                .username("test")
+                .firstName("Test")
+                .lastName("User")
+                .dob(dob)
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(request);
+
+        // WHEN, THEN
+        var response = mockMvc.perform(MockMvcRequestBuilders
+                    .post("/users")
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .content(content)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value("200"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username")
+                        .value("test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstName")
+                        .value("Test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastName")
+                        .value("User"));
+
+        log.info("Result: {}", response.andReturn().getResponse().getContentAsString());
+    }
+
+}
